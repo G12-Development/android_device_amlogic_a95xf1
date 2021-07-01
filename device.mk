4835427c03884d05e6511ebf7c948ea6c3b25a1e
Aaron Kling <webgeek1234@gmail.com>
2021-06-30 19:19:40 -0500

    beast: Configure Realtek Bluetooth/Wi-Fi HALs
    * We're not unifying things, and the stock MultiHAL requires
      framework side magic, so we're going with the hardware/realtek
      solution.
    
    * Set Bluetooth overlays based on stock.
    
    Change-Id: Ie8e0f2d2a5f1cb8337ebe88b903fb18a71602f18

--- device.mk
+++ device.mk
@@ -10,6 +10,14 @@ $(call inherit-product, vendor/google/beast/beast-vendor.mk)
 
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
 
+## Bluetooth
+BOARD_HAVE_BLUETOOTH_RTK_TV := true
+include hardware/realtek/rtkbt/rtkbt.mk
+
 ## Kernel Modules
 PRODUCT_PACKAGES += \
     8822bs
+
+## Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
